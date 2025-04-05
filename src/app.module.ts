98e343bc5f488052f63a3e987e01ea2d6d9740c7
sebasbit <sebastian.torres0426@gmail.com>
2025-04-05 18:18:18 -0500

    feat: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import validationSchema from './config/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
