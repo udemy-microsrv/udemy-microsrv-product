2c79ec08b1f5e36dd0e481226db733fad00d93be
sebasbit <sebastian.torres0426@gmail.com>
2025-04-05 19:42:45 -0500

    chore: log information

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(app.get(ConfigService).get('app.port') ?? 3000);
+  const port = app.get(ConfigService).get<number>('app.port') ?? 3000;
+  await app.listen(port);
+
+  const logger = new Logger('bootstrap');
+  logger.log(`Server is listening on port ${port}`);
 }
 bootstrap();
