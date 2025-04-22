60b48d6e36344dd8ed780efa65e4440db98f4c77
sebasbit <sebastian.torres0426@gmail.com>
2025-04-22 10:56:32 -0500

    fix: set default port to 3001

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,6 @@
 export default () => ({
   app: {
-    port: parseInt(process.env.PORT ?? '3000', 10),
+    port: parseInt(process.env.PORT ?? '3001', 10),
   },
   database: {
     url: process.env.DATABASE_URL,
