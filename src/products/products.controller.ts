01b9125d83efe0af9af2b5bcfd0b2f69defd77c8
sebasbit <sebastian.torres0426@gmail.com>
2025-05-02 10:45:15 -0500

    feat: use strings as message patterns

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,34 +9,34 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @MessagePattern({ cmd: 'product.create' })
+  @MessagePattern('products.create')
   create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
-  @MessagePattern({ cmd: 'product.find_all' })
+  @MessagePattern('products.find_all')
   findAll(@Payload() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
-  @MessagePattern({ cmd: 'product.find_one' })
+  @MessagePattern('products.find_one')
   findOne(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(id);
   }
 
-  @MessagePattern({ cmd: 'product.find_many' })
+  @MessagePattern('products.find_many')
   findMany(
     @Payload('ids', new ParseArrayPipe({ items: Number })) ids: number[],
   ) {
     return this.productsService.findMany(ids);
   }
 
-  @MessagePattern({ cmd: 'product.update' })
+  @MessagePattern('products.update')
   update(@Payload() updateProductDto: UpdateProductDto) {
     return this.productsService.update(updateProductDto);
   }
 
-  @MessagePattern({ cmd: 'product.delete' })
+  @MessagePattern('products.delete')
   remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
